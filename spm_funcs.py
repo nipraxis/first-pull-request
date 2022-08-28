e92c0833db8cc5d09c2aea8c769670370944c601
Apple <apple@MacBook-Pro-2.local>
2022-08-28 19:00:01 +0430

    changed spm_funcs

--- spm_funcs.py
+++ spm_funcs.py
@@ -25,37 +25,18 @@ import nipraxis
 
 
 def spm_global(vol):
-    """ Calculate SPM global metric for array `vol`
-
-    Parameters
-    ----------
-    vol : array
-        Array giving image data, usually 3D.
-
-    Returns
-    -------
-    g : float
-        SPM global metric for `vol`
-    """
     T = np.mean(vol) / 8
     return np.mean(vol[vol > T])
 
 
 def get_spm_globals(fname):
-    """ Calculate SPM global metrics for volumes in image filename `fname`
-
-    Parameters
-    ----------
-    fname : str
-        Filename of file containing 4D image
-
-    Returns
-    -------
-    spm_vals : array
-        SPM global metric for each 3D volume in the 4D image.
-    """
-    # +++your code here+++
-    # return
+    img = nib.load(fname)
+    data = img.get_fdata()
+    spm_vals = []
+    for i in range(data.shape[-1]):
+        vol = data[:, :, :, i]
+        spm_vals.append(spm_global(vol))
+    return spm_vals 
 
 
 def main():
